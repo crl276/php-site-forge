077043023b269f57379393847eba4c4698a4fce2
Callan Lamb <callan.lamb@gmail.com>
2017-01-09 08:42:01 -0500

    Use Eloquent ORM for Card relationship in CardsController

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use DB;
+use App\Card;
 use Illuminate\Http\Request;
 
 class CardsController extends Controller
@@ -10,7 +10,7 @@ class CardsController extends Controller
     //
     public function index() {
 
-    	$cards = DB::table('cards')->get();
+    	$cards = Card::all();
 
     	return view('cards.index', compact('cards'));
 
