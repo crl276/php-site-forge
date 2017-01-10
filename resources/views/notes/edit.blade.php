7b7a9a088c33cd6ec1c5e84fa7b934d295ed4f27
Callan Lamb <callan.lamb@gmail.com>
2017-01-10 07:42:50 -0500

    Add edit.blade.php

---
+++ resources/views/notes/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+
+
+	<h1>Edit the note</h1>
+
+    <form method="POST" action="/notes/{{ $note->id }}/">
+        {{ method_field('PATCH') }}
+        <div class="form-group">
+            <textarea name="body" class="form-control">{{ $note->body }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Update note</button>
+        </div>
+        {{ csrf_field() }}
+    </form>
+
+    <button class="btn btn-primary"><a href="/cards/{{ $note->card_id }}">Back to Card</a></button>
+
+    <style>
+        a {color: white;}
+    </style>
+
+
+
+
+@stop
