ff3779524a5d1d796435857b3ada69e4d45cf7ed
Callan Lamb <callan.lamb@gmail.com>
2017-01-11 21:05:07 -0500

    Spelling error on note

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -12,7 +12,7 @@ class NotesController extends Controller
 
     public function store(Request $request, Card $card)
     {
-    	// Call the addNote method on Card.php with a new Note passed as a paremeter, the contents of which is the entirety of the request
+    	// Call the addNote method on Card.php with a new Note passed as a parameter, the contents of which is the entirety of the request
     	$card->addNote(new Note($request->all()));
 
     	return back;
