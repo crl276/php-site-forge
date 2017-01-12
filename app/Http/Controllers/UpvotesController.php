4b4a078599191a15520bae807db85f37329247b1
Callan Lamb <callan.lamb@gmail.com>
2017-01-12 07:35:52 -0500

    Rename Upvote Controller to Vote Controller

--- app/Http/Controllers/UpvotesController.php
+++ app/Http/Controllers/UpvotesController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use App\Card;
 use Illuminate\Http\Request;
 
-class UpvotesController extends Controller
+class VotesController extends Controller
 {
     //
-	$upvotes = [0,1,2];
+	
 
 	public function upvote()
 	{
